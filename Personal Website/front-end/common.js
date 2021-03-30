8d83ceff0b4dac7fd4fe1f445efed3ea40246bd8
huxinche <70772950+huxinche@users.noreply.github.com>
2021-03-29 22:44:32 -0400

    Add files via upload

---
+++ Personal Website/front-end/common.js
@@ -0,0 +1,14 @@
+var currentUser = {
+}
+
+
+function getCurrentUser() {
+    return currentUser
+}
+
+
+function updateCurrentUser(user) {
+    currentUser = user
+}
+
+// export {getCurrentUser,updateCurrentUser}
